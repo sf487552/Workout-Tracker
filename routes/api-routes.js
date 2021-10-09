b143ffa07d1bdfa71ad5927bac80002303b7588f
Steve Fokas <sf487552@gmail.com>
2021-10-08 22:07:09 -0400

    asdg

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ const db = require('../models/');
 
 // get previous workouts
 router.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
+    db.workout.find({})
         .then(dbWorkout => {
             console.log(dbWorkout);
             res.json(dbWorkout);
@@ -15,7 +15,7 @@ router.get('/api/workouts', (req, res) => {
 
 // add new workout
 router.post('/api/workouts', ({ body }, res) => {
-    db.Workout.create(body)
+    db.workout.create(body)
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
@@ -26,7 +26,7 @@ router.post('/api/workouts', ({ body }, res) => {
 
 // update excercise by id
 router.put('/api/workouts/:id', (req, res) => {
-    db.Workout.updateOne(
+    db.workout.updateOne(
         { _id: req.params.id },
         { $push: { exercises: req.body } }
     )
@@ -39,7 +39,7 @@ router.put('/api/workouts/:id', (req, res) => {
 });
 // request last 7 workouts
 router.get('/api/workouts/range', (req, res) => {
-    db.Workout.find({})
+    db.workout.find({})
         .sort({ _id: -1 })
         .limit(7)
         .then(dbWorkout => {
